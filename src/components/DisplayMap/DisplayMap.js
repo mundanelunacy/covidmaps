4daa410aa519a63af2edd6845054b3b6eecc7f22
Satoshi Kawase <kawase@jhu.edu>
2020-03-24 00:24:19 +0900

    added covid icon

--- src/components/DisplayMap/DisplayMap.js
+++ src/components/DisplayMap/DisplayMap.js
@@ -2,14 +2,17 @@ import React from "react";
 import { Box, Typography } from "@material-ui/core";
 import { Map, InfoWindow, Marker } from "google-maps-react";
 
-export const DisplayMap = props => {
-    const { initialCenter, onMarkerClick, onMapClicked, centerMoved, displayMap, firebase, query } = props;
-
+export const DisplayMap = ({ initialCenter, onMarkerClick, onMapClicked, centerMoved, displayMap, firebase, query, google }) => {
     const onCenterMoved = (mapProps, map) => {
         centerMoved(mapProps, map, firebase);
     };
 
-    console.log(query.incidents);
+    const icon = {
+        url: "/covid_icon.png",
+        anchor: new google.maps.Point(32, 32),
+        scaledSize: new google.maps.Size(64, 64)
+    };
+
     return (
         <>
             <Box p={1} component="span">
@@ -17,9 +20,9 @@ export const DisplayMap = props => {
             </Box>
 
             <Box>
-                <Map google={props.google} zoom={14} center={query.center} onClick={onMapClicked} initialCenter={initialCenter} onDragend={onCenterMoved} style={{ width: "500px", height: "500px" }}>
+                <Map google={google} zoom={14} center={query.center} onClick={onMapClicked} initialCenter={initialCenter} onDragend={onCenterMoved} style={{ width: "500px", height: "500px" }}>
                     {query.incidents.map(incident => (
-                        <Marker key={incident.id} title={incident.name} name={incident.name} position={{ lat: incident.position.geopoint.latitude, lng: incident.position.geopoint.longitude }} onClick={onMarkerClick} />
+                        <Marker key={incident.id} title={incident.name} name={incident.name} position={{ lat: incident.position.geopoint.latitude, lng: incident.position.geopoint.longitude }} icon={icon} onClick={onMarkerClick} />
                     ))}
                     <InfoWindow marker={displayMap.activeMarker} visible={displayMap.showingInfoWindow}>
                         <div>
