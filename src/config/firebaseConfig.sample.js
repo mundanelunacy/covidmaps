931a058c01c5bdb124d024ae892bc949cd770634
Satoshi Kawase <kawase@jhu.edu>
2020-03-28 05:59:32 +0900

    added config sample files and setupInstructions

---
+++ src/config/firebaseConfig.sample.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDk6MSAl9EG46AIY-NZETvmbxJPO5XTAYc",
+    authDomain: "coronamaps-9e44a.firebaseapp.com",
+    databaseURL: "https://coronamaps-9e44a.firebaseio.com",
+    projectId: "coronamaps-9e44a",
+    storageBucket: "coronamaps-9e44a.appspot.com",
+    messagingSenderId: "22037141834",
+    appId: "1:22037141834:web:4bb92344f129a1d52949b3",
+    measurementId: "G-H2LR2QJ6XM"
+};
