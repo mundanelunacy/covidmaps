27233696e9f1f37102c9d934f056de57c75bb905
Satoshi Kawase <kawase@jhu.edu>
2020-03-28 20:18:43 +0900

    replaced real keys with placeholders

--- src/config/firebaseConfig.sample.js
+++ src/config/firebaseConfig.sample.js
@@ -1,10 +1,11 @@
+// enter key details here
 export const firebaseConfig = {
-    apiKey: "AIzaSyDk6MSAl9EG46AIY-NZETvmbxJPO5XTAYc",
-    authDomain: "coronamaps-9e44a.firebaseapp.com",
-    databaseURL: "https://coronamaps-9e44a.firebaseio.com",
-    projectId: "coronamaps-9e44a",
-    storageBucket: "coronamaps-9e44a.appspot.com",
-    messagingSenderId: "22037141834",
-    appId: "1:22037141834:web:4bb92344f129a1d52949b3",
-    measurementId: "G-H2LR2QJ6XM"
+    apiKey: "{apiKey}",
+    authDomain: "{authDomain}",
+    databaseURL: "{databaseURL}",
+    projectId: "projectId",
+    storageBucket: "sotrageBucket",
+    messagingSenderId: "messagingSenderId",
+    appId: "appId",
+    measurementId: "measurementId"
 };
