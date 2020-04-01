706c00bf4a6a7ddd0ea5129892cd57b03546450a
Satoshi Kawase <kawase@jhu.edu>
2020-04-01 13:06:16 +0900

    refactored some of the display code

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import axios from "axios";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
- export const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
+export const helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
+
+export const importKoreanData = functions.https.onRequest(async (request, response) => {
+    const original: any = await axios.get("https://coronamap.site/javascripts/ndata.js");
+
+    console.log(original.data);
+});
--- src/components/DisplayMap/DisplayMap.js
+++ src/components/DisplayMap/DisplayMap.js
@@ -1,155 +1,127 @@
-import React from 'react';
-import { Box, Typography } from '@material-ui/core';
-import { Map, InfoWindow, Marker } from 'google-maps-react';
-import moment from 'moment-timezone';
-
-// check for time distance and change marker color
-
-const checkTimestampDistance = (google, incident, onMarkerClick) => {
-  const now = new Date();
-  const markerDate = new Date(incident.startTimestampMs);
-  const incubationPeriod = 14;
-
-  let difference = now.getTime() - markerDate.getTime();
-
-  // calculate the difference in days
-  let daysDifference = Math.floor(difference / 1000 / 60 / 60 / 24);
-  difference -= daysDifference * 1000 * 60 * 60 * 24;
-
-  // check if days more than incubation period
-
-  if (daysDifference > 14) {
-    return;
-  }
-
-  var hoursDifference = Math.floor(difference / 1000 / 60 / 60);
-  difference -= hoursDifference * 1000 * 60 * 60;
-
-  var minutesDifference = Math.floor(difference / 1000 / 60);
-  difference -= minutesDifference * 1000 * 60;
-
-  const icon = {
-    url: '',
-    anchor: new google.maps.Point(12, 12),
-    scaledSize: new google.maps.Size(30, 30)
-  };
-
-  // change icon color based on time difference from now till incubation period
-
-  switch (true) {
-    case daysDifference > 4 && daysDifference <= incubationPeriod:
-      icon.url = '/markers/green_marker.png';
-      break;
-
-    case daysDifference <= 4 && daysDifference > 0:
-      icon.url = '/markers/yellow_marker.png';
-      break;
-
-    case daysDifference === 0 && hoursDifference <= 24:
-      icon.url = '/markers/red_marker.png';
-      break;
-
-    default:
-      break;
-  }
-
-  // return marker
-  return (
-    <Marker
-      key={incident.id}
-      title={incident.name}
-      name={incident.name}
-      start={incident.startTimestampMs}
-      end={incident.endTimestampMs}
-      color
-      position={{
-        lat: incident.position.geopoint.latitude,
-        lng: incident.position.geopoint.longitude
-      }}
-      onClick={onMarkerClick}
-      icon={icon}
-    />
-  );
-};
+import React from "react";
+import { Box, Typography } from "@material-ui/core";
+import { Map, InfoWindow, Marker } from "google-maps-react";
+import moment from "moment-timezone";
+const incubationPeriodDays = 14;
 
 export const DisplayMap = ({
-  initialCenter,
-  onMarkerClick,
-  onMapClicked,
-  centerMoved,
-  displayMap,
-  firebase,
-  query,
-  tzString,
-  queryIncidents
+    initialCenter,
+    onMarkerClick,
+    onMapClicked,
+    centerMoved,
+    displayMap,
+    firebase,
+    query,
+    tzString,
+    queryIncidents
 }) => {
-  const google = window.google;
-  const onCenterMoved = (mapProps, map) => {
-    centerMoved(mapProps, map, firebase);
-  };
+    const google = window.google;
+    const onCenterMoved = (mapProps, map) => {
+        centerMoved(mapProps, map, firebase);
+    };
 
-  const onMapInitialized = (mapProps, map) => {
-    map.setOptions({
-      minZoom: 15,
-      zoomControlOptions: { position: google.maps.ControlPosition.LEFT_BOTTOM }
-    });
+    const onMapInitialized = (mapProps, map) => {
+        map.setOptions({
+            minZoom: 15,
+            zoomControlOptions: { position: google.maps.ControlPosition.LEFT_BOTTOM }
+        });
 
-    if (initialCenter) {
-      queryIncidents(initialCenter.lat, initialCenter.lng, 2, firebase);
-      return;
-    }
+        if (initialCenter) {
+            queryIncidents(initialCenter.lat, initialCenter.lng, 2, firebase);
+            return;
+        }
 
-    if (query) {
-      queryIncidents(query.center.lat, query.center.lng, 2, firebase);
-      return;
-    }
-  };
+        if (query) {
+            queryIncidents(query.center.lat, query.center.lng, 2, firebase);
+            return;
+        }
+    };
 
-  return (
-    <>
-      <Map
-        google={google}
-        zoom={16}
-        center={query.center}
-        onClick={onMapClicked}
-        initialCenter={initialCenter}
-        onDragend={onCenterMoved}
-        streetViewControl={false}
-        mapTypeControl={false}
-        fullscreenControl={false}
-        onReady={onMapInitialized}
-        style={{ width: '100%', height: '100%' }}
-        containerStyle={{ position: 'static', width: '100%', height: '100%' }}
-      >
-        {query.incidents.map(incident =>
-          checkTimestampDistance(google, incident, onMarkerClick)
-        )}
-        <InfoWindow
-          marker={displayMap.activeMarker}
-          visible={displayMap.showingInfoWindow}
-        >
-          <>
-            <Box>
-              <Typography>{displayMap.selectedPlace.name}</Typography>
-            </Box>
-            <Box>
-              <Typography>
-                {moment
-                  .tz(displayMap.selectedPlace.start, tzString)
-                  .format('HH:mm Do-MMM-YYYY')}{' '}
-                (
-                {parseInt(
-                  (displayMap.selectedPlace.end -
-                    displayMap.selectedPlace.start) /
-                    1000 /
-                    60
-                )}{' '}
-                min)
-              </Typography>
-            </Box>
-          </>
-        </InfoWindow>
-      </Map>
-    </>
-  );
+    // check for time difference and change marker color
+    const filterMarkers = (google, incident, onMarkerClick) => {
+        const differenceSeconds = (new Date().getTime() - incident.startTimestampMs) / 1000;
+        const DAY = 24 * 60 * 60;
+        const icon = {
+            path: google.maps.SymbolPath.CIRCLE,
+            scale: 10,
+            fillColor: "#F00",
+            fillOpacity: 1,
+            strokeWeight: 0.4
+        };
+
+        // change icon color based on time difference from now till incubation period
+        switch (true) {
+            case differenceSeconds > 4 * DAY && differenceSeconds <= incubationPeriodDays * DAY:
+                icon.fillColor = "#00F"; //blue
+                break;
+            case differenceSeconds > DAY && differenceSeconds <= 4 * DAY:
+                icon.fillColor = "#FF0"; //yellow
+                break;
+            case differenceSeconds > 0 && differenceSeconds <= DAY:
+                icon.fillColor = "#F00"; //red
+                break;
+            default:
+                return;
+        }
+
+        // return marker
+        return (
+            <Marker
+                key={incident.id}
+                title={incident.name}
+                name={incident.name}
+                start={incident.startTimestampMs}
+                end={incident.endTimestampMs}
+                color
+                position={{
+                    lat: incident.position.geopoint.latitude,
+                    lng: incident.position.geopoint.longitude
+                }}
+                onClick={onMarkerClick}
+                icon={icon}
+            />
+        );
+    };
+
+    return (
+        <>
+            <Map
+                google={google}
+                zoom={16}
+                center={query.center}
+                onClick={onMapClicked}
+                initialCenter={initialCenter}
+                onDragend={onCenterMoved}
+                streetViewControl={false}
+                mapTypeControl={false}
+                fullscreenControl={false}
+                onReady={onMapInitialized}
+                style={{ width: "100%", height: "100%" }}
+                containerStyle={{ position: "static", width: "100%", height: "100%" }}
+            >
+                {query.incidents.map(incident => filterMarkers(google, incident, onMarkerClick))}
+                <InfoWindow marker={displayMap.activeMarker} visible={displayMap.showingInfoWindow}>
+                    <>
+                        <Box>
+                            <Typography>{displayMap.selectedPlace.name}</Typography>
+                        </Box>
+                        <Box>
+                            <Typography>
+                                {moment
+                                    .tz(displayMap.selectedPlace.start, tzString)
+                                    .format("HH:mm Do-MMM-YYYY")}{" "}
+                                (
+                                {parseInt(
+                                    (displayMap.selectedPlace.end - displayMap.selectedPlace.start) /
+                                        1000 /
+                                        60
+                                )}{" "}
+                                min)
+                            </Typography>
+                        </Box>
+                    </>
+                </InfoWindow>
+            </Map>
+        </>
+    );
 };
